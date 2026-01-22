47b763998bb58661edee192b9165586ca38c8e11
ray-209 <jowinray.209@gmail.com>
2026-01-23 03:52:37 +0800

    Add create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello, world"
-
-
-if __name__ == "__main__":
-    app.run()
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route("/")
+    def index():
+        return "Hello, world"
+    
+    return app
